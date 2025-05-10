c0f33eaa0f03d42c4aa16d22f890d51fb0f5775b
mohammad irfan <fleadomgreen@gmail.com>
2025-05-10 15:48:18 +0700

    memberikan fitur input teks pada index.html dan app.js

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 const body = document.body;
-        const output = document.getElementById('output');
+const output = document.getElementById("output");
 
-        //background color
-        document.getElementById('bgColorPicker').addEventListener('input', (e)=>{
-            body.style.backgroundColor = e.target.value;
-        });
+//background color
+document.getElementById("bgColorPicker").addEventListener("input", (e) => {
+  body.style.backgroundColor = e.target.value;
+});
 
-        document.getElementById('textColorPicker').addEventListener('input', (e)=>{
-            output.style.color = e.target.value;
-        });
+document.getElementById("textColorPicker").addEventListener("input", (e) => {
+  output.style.color = e.target.value;
+});
 
-        document.getElementById('fontSizeSlider').addEventListener('input', (e)=>{
-            output.style.fontSize = e.target.value + 'px';
-        });
+document.getElementById("fontSizeSlider").addEventListener("input", (e) => {
+  output.style.fontSize = e.target.value + "px";
+});
 
-        document.getElementById('FontFamilySelector').addEventListener('change', (e)=>{
-            output.style.fontFamily = e.target.value;
-        });
+document
+  .getElementById("FontFamilySelector")
+  .addEventListener("change", (e) => {
+    output.style.fontFamily = e.target.value;
+  });
 
-        document.getElementById('darkModeToggle').addEventListener('change', (e)=>{
-            if(e.target.checked){
-                body.style.backgroundColor = '#222';
-                body.style.color = '#f0f0f0';
-            }else{
-                body.style.backgroundColor = 'white';
-                body.style.color = 'black';
-            }
-        });
+document.getElementById("darkModeToggle").addEventListener("change", (e) => {
+  if (e.target.checked) {
+    body.style.backgroundColor = "#222";
+    body.style.color = "#f0f0f0";
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  }
+});
+
+document.getElementById("textInput").addEventListener("input", (e) => {
+  output.textContent =
+    e.target.value || "ini adalah teks yang akan berubah gaya";
+});
